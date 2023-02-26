7da8d81cd6abfc7bc6c3a2945d28b265fd9dd1a3
João Pedro C Jardim <joaopedrocjardim@gmail.com>
2023-02-26 15:15:18 -0300

    Inicio da estilização da pagina experiencia-socio

--- script/script.js
+++ script/script.js
@@ -1,9 +1,36 @@
 const btnMenus = document.querySelector(".btn-loja-header-menu")
 const menu = document.querySelector(".loja-header-menu")
 
-console.log(menu)
-
  function ativarMenu(){
     menu.classList.toggle('ativo')
 }
-btnMenus.addEventListener('click', ativarMenu) 
+if(btnMenus){
+    btnMenus.addEventListener('click', ativarMenu) 
+
+}
+
+
+
+
+const btnPontuar = document.querySelector("#btnPontuar")
+const btnCatalogo = document.querySelector("#btnCatalogo")
+const containerPontuar = document.querySelector(".s-e-container")
+const containerCatalogo = document.querySelector(".s-ec-container")
+
+console.log(btnCatalogo)
+console.log(btnPontuar)
+
+function ativarPontuar(){
+    containerPontuar.classList.add('ativo')
+    containerCatalogo.classList.remove('ativo')
+}
+function ativarCatalogo(){
+    containerCatalogo.classList.add('ativo')
+    containerPontuar.classList.remove('ativo')
+}
+
+btnPontuar.addEventListener('click', ativarPontuar)
+btnCatalogo.addEventListener('click', ativarCatalogo)
+
+
+
